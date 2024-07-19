b7b9319fb2e1b562f44981ffa3424b2cdb5c8e00
Brian <briancmuhammad@yahoo.com>
2024-07-19 19:48:00 -0400

    started using Api integration with axios

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,5 +1,24 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 function Home() {
-    return <h1>Home</h1>
+  const [users, setUsers] = useState([]);
+  // return <h1>{users.length > 0 &&  users[0].name}</h1>; short than terinary
+  async function fetchUsers() {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    setUsers(data);
+  }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+  return (
+    <div>
+      <h1>{users[0]?.name}</h1>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
